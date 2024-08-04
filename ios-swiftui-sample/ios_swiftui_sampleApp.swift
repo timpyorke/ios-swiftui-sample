ce3aa0ea2b7a1c27e4bb9d273dfcc9c7e81144b1
Timp Yorke <thongphitak.tim@gmail.com>
2024-08-04 09:50:47 +0700

    Initial Commit

---
+++ ios-swiftui-sample/ios_swiftui_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_swiftui_sampleApp.swift
+//  ios-swiftui-sample
+//
+//  Created by Thongphitak Sowanna on 4/8/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct ios_swiftui_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
